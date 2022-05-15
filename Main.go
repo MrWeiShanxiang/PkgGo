33dd3c7e3fa049fc93b55aef9b4ea72c4b976bce
MrWeiShanxiang <wsx1777485505@163.com>
2022-05-15 19:39:32 +0800

    change url

---
+++ Main.go
@@ -0,0 +1,7 @@
+package main
+
+import "PkgGo/FetchRepostory"
+
+func main() {
+	FetchRepostory.Fetch("https://s.cn.bing.net/th?id=OJ.ctIMyEUgdeHZwQ&w=120&h=160&c=8&rs=1&pid=academic", "./j.jpg")
+}
